a599618d9d1fca49546b53831ea594a36b506657
Yana <iana.markovets@fingers.by>
2023-03-21 19:36:03 +0100

    layout CV

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import './App.css';
+import React from 'react';
+import data from "./data.js"
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+        <div className="App--CV">
+          <div className="App--CV-container">
+            <header className="App--CV-header">
+              <h1>{data.bio.name}</h1>
+              <div className="App--header-infoBox">
+                <h2>{data.bio.years}</h2>
+                <span>{data.bio.location}</span>
+                <span>{data.bio.email}</span>
+              </div>
+              <p>{data.bio.info}</p>
+            </header>
+            <main className="App--CV-main">
+              <section>
+                <h3>Experience</h3>
+                {data.experience.map(place => {
+                  return (
+                      <div key={place.name} className="App--experience">
+                        <div className="App--experience-place">
+                          <h4>{place.name}</h4>
+                          <span>{place.date}</span>
+                        </div>
+                        <div className="App--experience-desc">
+                          <p>{place.description}</p>
+                          <p>Stack: {place.stack}</p>
+                        </div>
+                      </div>
+                  )
+                })}
+              </section>
+              <section>
+                <h3>Education</h3>
+                {data.education.map(place => (
+                    <div key={place.university} className="App--education">
+                      <span>{place.period}</span>
+                      <h4>{place.university}</h4>
+                      <p>{place.degree}</p>
+                    </div>
+                ))}
+                <div className="App--education-skills">
+                  <h3>Skills</h3>
+                  <ul>
+                    {data.skills.map(skill => (<li key={skill}>{skill}</li>))}
+                  </ul>
+                </div>
+              </section>
+            </main>
+          </div>
+        </div>
+      </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    "bio": {
+        "name": "Yana Markovets",
+        "position": "Frontend developer",
+        "email": "yana.mars.pl@gmail.com",
+        "info": "Hello, I’m a frontend developer with 3 years of experience, with a great desire to grow and develop in the frontend. I’m a good team player with upper-intermediate English. Also I keep improving my skills on Udemy and I'm open to new challenges and interesting projects!",
+        "years": "3+",
+        "location": "Warsaw, Poland"
+    },
+    "social":{
+        linkedin: "https://www.linkedin.com/in/yana-markovets/"
+    },
+    "experience": [
+        {
+            "name": "Reductions Privees",
+            "stack": "NextJS, Redux, i18n, Highcharts, Slick, SCSS, Typescript, GraphQL, Hygraph, Jest",
+            "description": "Reductions Privees is a system for providing discounts for customers and businesses with full information tracking. Full redesign of one of the application.",
+            "date": "2022  - Present",
+        },
+        {
+            "name": "Leedi",
+            "stack": "Next.JS, MobX, GraphQL, i18n, TypeScript , Yup, UseForm, Jest, Recharts",
+            "description": "Leedi is a tool for systematization, management and accumulation of leads base for various campaigns conducted by clients. Group of applications. Creating custom website SDK to work with adv.",
+            "date": "2021  - Present",
+        },
+        {
+            "name": "Aibon Technologies",
+            "stack": "React, custom library of control Redux, Lodash, Reselect, Recompose, Figma, Git, Yup, Formik, Styled Component, OpenSeadragon",
+            "description": "Aibion Technologies is a product company which owns a tool for storing, annotating and analyzing digital medical images.Was participating in setting up website aibion.ai using Nunjucks and Less.",
+            "date": "2020  -  2021",
+        }
+    ],
+    "education": [
+        {
+            id: 0,
+            "university": "ITMO, Saint-Petersburg",
+            "period": "2018 - 2019",
+            "degree": "Requalification course in Computer science and user experience design"
+        },
+        {
+            id: 1,
+            "university": "BSEU, Minsk",
+            "period": "2011 - 2016",
+            "degree": "Bachelor Degree in Intercultural Business Communication"
+        }
+    ],
+    "skills": [
+        "HTML",
+        "CSS/SCSS",
+        "JavaScript",
+        "React",
+        "NextJS",
+        "RestAPI",
+        "GraphQL",
+        "Hygraph",
+        "Highcharts",
+        "Recharts",
+        "Redux",
+        "MobX",
+        "Yup",
+        "UseForm",
+        "Formik",
+        "TypeScript",
+        "Styled Component",
+        "Material UI",
+        "Figma",
+        "Email Templates"
+    ]
+}
+
+export default data
